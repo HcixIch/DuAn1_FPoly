0bc76cf4c33fd804dfb7a1c9d15a06837e10c48e
HcixIch <ichndps41009@gmail.com>
2024-11-10 09:12:40 +0700

    Update m_product.php

--- models/m_product.php
+++ models/m_product.php
@@ -7,9 +7,14 @@ class Product extends Database
     {
         $this->db = new Database();
     }
-    public function getAllProduct()
+    public function getProductByCodition($cate,$new,$hot,$sale,$limit);
     {
-        $sql = "SELECT * FROM product";
+        $sql = "SELECT * FROM product WHERE";
+        if($cate!=0&&$cate!=""){
+            $sql.=" id_category = $cate";
+        }
+        if($new==1){
+            $sql.=" AND new = 1";
+        }
         return $this->db->query($sql);
     }
-}
