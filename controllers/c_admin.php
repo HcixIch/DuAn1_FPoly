ab2d8f6292ce7795797d717d1478230a1b9d8623
Nguyen-Hai-Ha <hanguyen032325@gmail.com>
2024-11-19 09:53:31 +0700

    .

--- controllers/c_admin.php
+++ controllers/c_admin.php
@@ -1,19 +1,17 @@
 <?php
+include_once '.views_admin/header.php';
     if (isset($_GET['view'])){
         switch ($_GET['view']) {
             case 'home':
                 include_once './model/m_Database.php';
                 include_once './model/m_sanpham.php';
-                include_once '.views_admin/header.php';
                 include_once '.views_admin/home.php';
-                include_once '.views_admin/footer.php';
                 break;
             default:
                 include_once './model/m_Database.php';
                 include_once './model/m_sanpham.php';
-                include_once '.views_admin/header.php';
                 include_once '.views_admin/home.php';
-                include_once '.views_admin/footer.php';
         }
     }
+include_once '.views_admin/footer.php';
 ?>
