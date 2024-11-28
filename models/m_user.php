e1dc7ace034c8459e7b402b074614beb7390e99b
duynhps38244 <duynhps38244@gmail.com>
2024-11-28 10:16:15 +0700

    Update m_user.php

--- models/m_user.php
+++ models/m_user.php
@@ -40,10 +40,6 @@ class User extends Database
         $sql = "SELECT * FROM user WHERE email_user ='".$email."' AND password = '".$password."' ";
         return $this->db->getAll($sql);
     }
-<
+    
     
 }
-
-
-?>
-
