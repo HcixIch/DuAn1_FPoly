d937bb4ad7e1d19965d644628b40b69be9b97166
duynhps38244 <duynhps38244@gmail.com>
2024-11-30 10:14:10 +0700

    Update addpro.php

--- viewsadmin/addpro.php
+++ viewsadmin/addpro.php
@@ -3,7 +3,6 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
-<<<<<<< Updated upstream
                         <div class="header">
                         <h4 class="title">Danh sách sản phẩm</h4>
                         <p class="category">Quản lý tất cả sản phẩm</p>
@@ -16,7 +15,6 @@
                                     <th>Số lượng</th>
                                     <th>Mô tả</th>
                                     <th>Sửa/Xóa</th>
-=======
                             <div class="header">
                                 <h4 class="title">Thêm sản phẩm</h4>
                                 <p class="category"></p>
@@ -24,8 +22,7 @@
                             <div class="content table-responsive table-full-width">
                                 <form>
                                     <label for="">Danh mục sản phẩm</label>
-                                    <select name="cate" id="cate" class="form-control"></select>
-                                        
+                                    <select name="cate" id="cate" class="form-control"></select>    
                                     <label for="">Tên sản phẩm</label>
                                     <input type="text" name="name" id="name" class="form-control"> 
                                     <label for="">Giá sản phẩm</label>
@@ -38,8 +35,6 @@
                                     <input type="file" name="image" id="image" class="form-control">
                                     <input type="button" value="Thêm sản phẩm" onclick="">
                                 </form>
->>>>>>> Stashed changes
-
                                 </tr>
                             </thead>
                             <tbody>
@@ -53,7 +48,6 @@
                                             <a href="?ctrl=admin&view=edituser&id=<?= $user['id_user'] ?>">Sửa</a> |
                                             <a href="?ctrl=user&action=deleteuser&id=<?= $user['id_user'] ?>" onclick="return confirm('Bạn có chắc muốn xóa?')">Xóa</a>
                                         </td>
-
                                     </tr>
                                 <?php } ?>
                             </tbody>
@@ -64,12 +58,10 @@
                         <h4 class="title">Thêm sản phẩm</h4>
                         <p class="category"></p>
                     </div>
-
                     <div class="content table-responsive table-full-width">
                         <form>
                             <label for="">Sản phẩm</label>
                             <select name="cate" id="cate" class="form-control"></select>
-
                             <option value="$id">$name</option>
                             <label for="">Tên sản phẩm</label>
                             <input type="text" name="name" id="name" class="form-control" > 
