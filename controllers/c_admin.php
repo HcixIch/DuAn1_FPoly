6196e4bda7de4fa25790d6322cf89fe0dffdd1ba
MinkFat <minhphatmp555@gmail.com>
2024-11-30 09:33:48 +0700

    .

--- controllers/c_admin.php
+++ controllers/c_admin.php
@@ -13,9 +13,20 @@ if (isset($_GET['view'])) {
             $user_list = $user->getAllUser();
             include './viewsadmin/user.php';
             break;
-        case 'editcate':
-            include_once './controllers/category_handler.php';
-            break;
+            case 'editcate':
+                if (isset($_GET['id'])) {
+                    $id = $_GET['id'];
+                    $category = $cates->getCategoryById($id);
+                    if (isset($_POST['update_category'])) {
+                        $name_category = $_POST['name_category'];
+                        $cates->updateCategory($id, $name_category);
+                        header("Location: ?ctrl=admin");
+                        exit();
+                    }
+                }
+                include './viewsadmin/editcate.php';
+                break;
+            
     }
 } else {
     $Allcates = $cates->getAllCategories();
