d6f89339aa98f08281694276021ac7ebbe41c234
MinkFat <minhphatmp555@gmail.com>
2024-11-30 09:27:20 +0700

    .

--- controllers/c_admin.php
+++ controllers/c_admin.php
@@ -1,27 +1,5 @@
 <?php
 include_once './viewsadmin/header.php';
-<<<<<<< Updated upstream
-    if (isset($_GET['view'])){
-        switch ($_GET['view']) {
-            case 'addpro':
-                $addpro_list = $prod->getAllProducts();
-                include_once './viewsadmin/addpro.php';
-                break;
-            case 'order':
-                $order_list = $order->getAllOrder();
-                include './viewsadmin/order.php';
-                break;
-            case 'user':
-                $user_list = $user->getAllUser();
-                include './viewsadmin/user.php';
-                break;
-            
-        }
-    }
-    else{
-            $Allcates = $cates->getAllCategories();
-            include_once './viewsadmin/home.php';
-=======
 if (isset($_GET['view'])) {
     switch ($_GET['view']) {
         case 'addpro':
@@ -38,7 +16,6 @@ if (isset($_GET['view'])) {
         case 'editcate':
             include_once './controllers/category_handler.php';
             break;
->>>>>>> Stashed changes
     }
 } else {
     $Allcates = $cates->getAllCategories();
