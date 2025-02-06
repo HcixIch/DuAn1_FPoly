eeb0caf245238e0eb7b277242beaacbde3269e2e
HcixIch <ichndps41009@gmail.com>
2025-02-06 23:43:08 +0700

    Update user.php

--- viewsadmin/user.php
+++ viewsadmin/user.php
@@ -23,30 +23,31 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php foreach($user_list as $user){ ?>
-                                    <?php if($user['id_user'] != $_SESSION['user'][0]['id_user']){ ?>
-                                    <form action="" method="post">
-                                        <tr style="<?php if ($user['role'] == 2) echo "display:none" ?>">
-                                            <td><?= $user['id_user'] ?></td>
-                                            <td><?= $user['full_name'] ?></td>
-                                            <td><?= $user['phone_user'] ?></td>
-                                            <td><?= $user['email_user'] ?></td>
-                                            <?php if($_SESSION['user'][0]['role'] == 2 ){ ?>
-                                                <?php if($user['role'] == 1){ ?>
-                                                    <td>
-                                                        <input type="hidden" name="id" value="<?= $user['id_user'] ?>">
-                                                        <button name="Del" class="btn btn-primary">Thu hồi quyền</button>
-                                                    </td>
-                                                <?php } else { ?>
-                                                    <td>
-                                                        <input type="hidden" name="id" value="<?= $user['id_user'] ?>">
-                                                        <button name="submit" class="btn btn-primary">Cấp Quyền</button>
-                                                    </td>
-                                                <?php } ?>
-                                        </tr>
-                                    </form>
+                                <?php foreach ($user_list as $user) { ?>
+                                    <?php if ($user['id_user'] != $_SESSION['user'][0]['id_user']) { ?>
+                                        <form action="" method="post">
+                                            <tr style="<?php if ($user['role'] == 2) echo "display:none" ?>">
+                                                <td><?= $user['id_user'] ?></td>
+                                                <td><?= $user['full_name'] ?></td>
+                                                <td><?= $user['phone_user'] ?></td>
+                                                <td><?= $user['email_user'] ?></td>
+                                                <?php if ($_SESSION['user'][0]['role'] == 2) { ?>
+                                                    <?php if ($user['role'] == 1) { ?>
+                                                        <td>
+                                                            <input type="hidden" name="id" value="<?= $user['id_user'] ?>">
+                                                            <button name="Del" class="btn btn-primary">Thu hồi quyền</button>
+                                                        </td>
+                                                    <?php } else { ?>
+                                                        <td>
+                                                            <input type="hidden" name="id" value="<?= $user['id_user'] ?>">
+                                                            <button name="submit" class="btn btn-primary">Cấp Quyền</button>
+                                                        </td>
+                                                    <?php } ?>
+                                            </tr>
+                                        </form>
                                     <?php } ?>
-                                <?php } ?>
+                            <?php }
+                                } ?>
                             </tbody>
                         </table>
                     </div>
